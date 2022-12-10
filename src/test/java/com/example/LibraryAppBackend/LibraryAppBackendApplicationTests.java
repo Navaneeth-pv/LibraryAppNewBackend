7c343b11e4e1fe9830d6805048270943c2915c3f
Navaneeth-pv <navaneethpv902@gmail.com>
2022-12-10 19:08:58 +0530

    first Commit

---
+++ src/test/java/com/example/LibraryAppBackend/LibraryAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibraryAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
